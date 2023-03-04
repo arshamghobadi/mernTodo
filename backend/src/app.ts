72456ba840b43e3365edc3b6176ac906648f87f5
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-03-04 23:12:39 +0000

    sucess post request from postman

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,20 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import notesRoutes from './routes/notes';
 const app = express();
+app.use(express.json());
+app.use('/api/notes', notesRoutes);
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.use((req, res, next) => {
+  next(Error('Endpoint not found'));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = 'An unknow erroe occurred';
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
