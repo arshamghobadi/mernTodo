094629329a27de61d95c2f3713f68d5300522e22
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-03-05 16:19:58 +0000

    patch and delete note router and handle

--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -3,6 +3,8 @@ import * as NotesController from '../controllers/notes';
 const router = express.Router();
 router.get('/', NotesController.getNotes);
 router.get('/:noteId', NotesController.getnote);
+router.patch('/:noteId', NotesController.updatenote);
 router.post('/', NotesController.createNote);
+router.delete('/:noteId', NotesController.deletNote);
 
 export default router;
