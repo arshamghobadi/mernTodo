98ded3bcdab2e2ec00ed2d4998566ad62e70815e
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-03-05 13:18:54 +0000

    improve routing api

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -8,6 +8,15 @@ export const getNotes: RequestHandler = async (req, res, next) => {
     next(error);
   }
 };
+export const getnote: RequestHandler = async (req, res, next) => {
+  const noteId = req.params.noteId;
+  try {
+    const note = await NoteModel.findById(noteId).exec();
+    res.status(200).json(note);
+  } catch (error) {
+    next(error);
+  }
+};
 export const createNote: RequestHandler = async (req, res, next) => {
   const title = req.body.title;
   const text = req.body.text;
--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as NotesController from '../controllers/notes';
 const router = express.Router();
 router.get('/', NotesController.getNotes);
+router.get('/:noteId', NotesController.getnote);
 router.post('/', NotesController.createNote);
 
 export default router;
