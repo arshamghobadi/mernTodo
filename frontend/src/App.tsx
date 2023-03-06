e272d32187bfd086ff3ec3be0160925819c4c59c
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-03-06 21:18:54 +0000

    add button to create list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import AddNoteDialog from './components/AddNoteDialog';
 
 import NoteUi from './components/NoteUi';
 import * as NotesApi from './network/notes_api';
@@ -6,6 +7,7 @@ import { Note as NoteModel } from './typing/note';
 
 function App() {
   const [note, setNote] = useState<NoteModel[]>([]);
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
 
   useEffect(() => {
     async function loadNotes() {
@@ -22,10 +24,27 @@ function App() {
 
   return (
     <div className=" h-auto bg-gray-200 ">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => setShowAddNoteDialog(true)}
+      >
+        Add new note
+      </button>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full  gap-5 p-10 max-w-6xl mx-auto lg:h-screen">
         {note.map((item) => (
           <NoteUi key={item._id} note={item} />
         ))}
+      </div>
+      <div>
+        {showAddNoteDialog && (
+          <AddNoteDialog
+            onDismiss={() => setShowAddNoteDialog(false)}
+            onNoteSaved={(newNote) => {
+              setNote([...note, newNote]);
+              setShowAddNoteDialog(false);
+            }}
+          />
+        )}
       </div>
     </div>
   );
---
+++ frontend/src/components/AddNoteDialog.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { NoteInput } from '../network/notes_api';
+import * as NotesApi from '../network/notes_api';
+import { Note } from '../typing/note';
+
+type Props = {
+  onDismiss: () => void;
+  onNoteSaved: (note: Note) => void;
+};
+
+export default function AddNoteDialog({ onDismiss, onNoteSaved }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<NoteInput>();
+  async function onSubmit(input: NoteInput) {
+    try {
+      const noteResponse = await NotesApi.createNote(input);
+      onNoteSaved(noteResponse);
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  }
+  return (
+    <form
+      id="addNoteForm"
+      onSubmit={handleSubmit(onSubmit)}
+      className="fixed inset-0 z-50 flex items-center justify-center overflow-auto bg-black bg-opacity-50"
+    >
+      <div className="relative z-50 w-1/2 max-w-2xl p-6 mx-auto bg-white rounded-md shadow-lg">
+        <div className="flex flex-col justify-between items-center mb-4 space-y-3">
+          <input
+            {...register('title', { required: 'Required' })}
+            placeholder="Title"
+            type="text"
+            className="text-lg font-semibold border-2 w-full"
+          />
+          <div typeof="">{errors.title?.message}</div>
+          <textarea
+            {...register('text')}
+            placeholder="Text"
+            className="text-sm font-semibold border-2 w-full"
+          />
+        </div>
+        <div className="mb-4">{/* Add your modal content here */}</div>
+        <div className="flex justify-end">
+          <button
+            className="bg-gray-500 hover:bg-gray-700 text-white font-bold p-1 md:p-2 rounded mr-2"
+            onClick={onDismiss}
+          >
+            Close
+          </button>
+          <button
+            disabled={isSubmitting}
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1 md:p-2 rounded"
+          >
+            Save changes
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
