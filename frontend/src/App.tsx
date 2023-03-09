144cb9f627a08e069c38fbddfecb73cfe53fee3d
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-03-09 20:49:29 +0000

    auth and handle eerors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,111 +1,55 @@
 import { useEffect, useState } from 'react';
-import AddNoteDialog from './components/AddNoteDialog';
-import { Blocks } from 'react-loader-spinner';
-import NoteUi from './components/NoteUi';
 import * as NotesApi from './network/notes_api';
-import { Note as NoteModel } from './typing/note';
+import LoginModel from './components/LoginModel';
+import Navbar from './components/navbar';
+import SignUpModel from './components/SignUpModel';
+
+import { User } from './typing/user';
 
 function App() {
-  const [notes, setNote] = useState<NoteModel[]>([]);
-  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
-  const [notesLoading, setNotesLoading] = useState(true);
-  const [showNotesLoadingError, setShowNotesLoadingError] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
+  const [showSignUpModel, setShowSignUpModel] = useState(false);
+  const [showLoginModel, setShowLoginModel] = useState(false);
 
-  const [noteToEdit, setNoteToEdit] = useState<NoteModel | null>(null);
   useEffect(() => {
-    async function loadNotes() {
+    async function fetchLoggedInUser() {
       try {
-        setShowNotesLoadingError(false);
-        setNotesLoading(true);
-        const notes = await NotesApi.fetchNotes();
-        setNote(notes);
+        const user = await NotesApi.getLoggedInUser();
+        setLoggedInUser(user);
       } catch (error) {
         console.error(error);
-        setShowNotesLoadingError(true);
-      } finally {
-        setNotesLoading(false);
       }
     }
-    loadNotes();
-  }, []);
-  async function deleteNote(note: NoteModel) {
-    try {
-      await NotesApi.deleteNote(note._id);
-      setNote(notes.filter((existingNote) => existingNote._id !== note._id));
-    } catch (error) {
-      console.error(error);
-      alert(error);
-    }
-  }
+    fetchLoggedInUser();
+  });
 
   return (
     <div className=" h-auto bg-gray-200 ">
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => setShowAddNoteDialog(true)}
-      >
-        Add new note
-      </button>
-      {notesLoading && (
-        <Blocks
-          visible={true}
-          height="80"
-          width="80"
-          ariaLabel="blocks-loading"
-          wrapperStyle={{}}
-          wrapperClass="blocks-wrapper"
+      <Navbar
+        loggedInUser={loggedInUser}
+        onLoginClicked={() => setShowLoginModel(true)}
+        onSignUpClicked={() => setShowSignUpModel(true)}
+        onLogoutSuccessful={() => setLoggedInUser(null)}
+      />
+
+      {showSignUpModel && (
+        <SignUpModel
+          onDismiss={() => setShowSignUpModel(false)}
+          onSignUpSuccessful={(user) => {
+            setLoggedInUser(user);
+            setShowSignUpModel(false);
+          }}
         />
       )}
-      {showNotesLoadingError && (
-        <p>Somthing went wrong/please refresh the page</p>
+      {showLoginModel && (
+        <LoginModel
+          onDismiss={() => setShowLoginModel(false)}
+          onSignUpSuccessful={(user) => {
+            setLoggedInUser(user);
+            setShowLoginModel(false);
+          }}
+        />
       )}
-      {!notesLoading && !showNotesLoadingError && (
-        <>
-          {notes.length > 0 ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full  gap-5 p-10 max-w-6xl mx-auto lg:h-screen">
-              {notes.map((item) => (
-                <NoteUi
-                  onDeleteNoteClicked={deleteNote}
-                  key={item._id}
-                  note={item}
-                  onNoteClicked={setNoteToEdit}
-                />
-              ))}
-            </div>
-          ) : (
-            <p>You dont have any notes yet</p>
-          )}
-        </>
-      )}
-
-      <div>
-        {showAddNoteDialog && (
-          <AddNoteDialog
-            onDismiss={() => setShowAddNoteDialog(false)}
-            onNoteSaved={(newNote) => {
-              setNote([...notes, newNote]);
-              setShowAddNoteDialog(false);
-            }}
-          />
-        )}
-
-        {noteToEdit && (
-          <AddNoteDialog
-            noteToEdit={noteToEdit}
-            onDismiss={() => setNoteToEdit(null)}
-            onNoteSaved={(updateNote) => {
-              setNote(
-                notes.map((existingNote) =>
-                  existingNote._id === updateNote._id
-                    ? updateNote
-                    : existingNote
-                )
-              );
-              setNoteToEdit(null);
-            }}
-          />
-        )}
-      </div>
     </div>
   );
 }
