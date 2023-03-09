38f51677bad7797c3e6ce0db4f08d36114fa5ca8
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-03-09 21:04:44 +0000

    react route dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,10 @@ import Navbar from './components/navbar';
 import SignUpModel from './components/SignUpModel';
 
 import { User } from './typing/user';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
+import NotesPage from './pages/NotesPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
@@ -24,33 +28,39 @@ function App() {
   });
 
   return (
-    <div className=" h-auto bg-gray-200 ">
-      <Navbar
-        loggedInUser={loggedInUser}
-        onLoginClicked={() => setShowLoginModel(true)}
-        onSignUpClicked={() => setShowSignUpModel(true)}
-        onLogoutSuccessful={() => setLoggedInUser(null)}
-      />
+    <BrowserRouter>
+      <div className=" h-auto bg-gray-200 ">
+        <Navbar
+          loggedInUser={loggedInUser}
+          onLoginClicked={() => setShowLoginModel(true)}
+          onSignUpClicked={() => setShowSignUpModel(true)}
+          onLogoutSuccessful={() => setLoggedInUser(null)}
+        />
+        <Routes>
+          <Route path="/" element={<NotesPage loggedInUser={loggedInUser} />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
 
-      {showSignUpModel && (
-        <SignUpModel
-          onDismiss={() => setShowSignUpModel(false)}
-          onSignUpSuccessful={(user) => {
-            setLoggedInUser(user);
-            setShowSignUpModel(false);
-          }}
-        />
-      )}
-      {showLoginModel && (
-        <LoginModel
-          onDismiss={() => setShowLoginModel(false)}
-          onSignUpSuccessful={(user) => {
-            setLoggedInUser(user);
-            setShowLoginModel(false);
-          }}
-        />
-      )}
-    </div>
+        {showSignUpModel && (
+          <SignUpModel
+            onDismiss={() => setShowSignUpModel(false)}
+            onSignUpSuccessful={(user) => {
+              setLoggedInUser(user);
+              setShowSignUpModel(false);
+            }}
+          />
+        )}
+        {showLoginModel && (
+          <LoginModel
+            onDismiss={() => setShowLoginModel(false)}
+            onSignUpSuccessful={(user) => {
+              setLoggedInUser(user);
+              setShowLoginModel(false);
+            }}
+          />
+        )}
+      </div>
+    </BrowserRouter>
   );
 }
 
